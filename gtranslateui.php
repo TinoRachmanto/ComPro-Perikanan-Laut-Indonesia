39995665cf59a073121ccb8d5350671e3e7836bd
Macbook <macbook@Macbooks-MacBook-Pro.local>
2023-11-05 08:01:04 +0700

    commit first

---
+++ gtranslateui.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+   body{background-color:#eee;}
+   .container_gtranslate{
+     background-color:#ccc;
+     padding:5px;
+     text-align:right;
+   }
+</style>
+</head>
+<body>
+   <div class="container_gtranslate">
+   Terjemahkan halaman ini:
+   <div id="google_translate_element"></div>
+   </div>
+   <script type="text/javascript">
+   function googleTranslateElementInit() {
+     new google.translate.TranslateElement({pageLanguage: 'id'}, 'google_translate_element');
+   }
+   </script>
+   <script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
+</body>
+</html>
